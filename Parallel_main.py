fb95efe198d67f3bbf18ff7dd71edc02fabccdc0
qingluMeng <57898734+qingluM@users.noreply.github.com>
2024-04-10 17:00:26 +0800

    Add files via upload

---
+++ Parallel_main.py
@@ -0,0 +1,278 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 12 14:11:57 2023
+
+@author: Mxm
+"""
+import os
+import torch
+import time
+import scipy.io as sio
+import numpy as np
+import pandas as pd
+from utils import NORM_Net,Z_score, L2Loss, get_parameter_number
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
+def main(args):
+    
+    print("\n=============================")
+    print("torch.cuda.is_available(): " + str(torch.cuda.is_available()))
+    if torch.cuda.is_available():
+        print("torch.cuda.get_device_name(0): " + str(torch.cuda.get_device_name(0)))
+    print("=============================\n")
+    
+    path = args.data_dir
+    
+    CaseName = args.casename
+    
+    learning_rate = args.lr
+    batch_size = args.batch_size
+    epochs = args.epochs
+    
+    ntrain = args.num_train
+    ntest = args.num_test
+    
+    modes  = args.modes     # 时间维度的截断数量
+    Lmodes = args.LBOmodes  # NORM中LBO基的截断数量
+    width  =  args.width
+    
+    BASIS = args.BASIS
+    
+    step_size = 100
+    gamma = 0.5
+    
+    Train_Data = sio.loadmat(path)
+    
+    y_dataIn = Train_Data['U_field']
+    x_dataIn = Train_Data['U_initial']
+
+    # y_dataIn = y_dataIn.transpose(0,2,1)
+    print("要进行基分解的数据的维度：",y_dataIn.shape)
+    print("***如果是求空间维度的基，请将数据调整为：num * Nt * Nx")
+    print("***如果是求时间维度的基，请将数据调整为：num * Nx * Nt")
+    
+    nt  = y_dataIn.shape[2]
+    nx  = y_dataIn.shape[1]
+
+    POD_BASIS = sio.loadmat('../data/POD_basis_norm.mat') 
+    LBO_BASIS = sio.loadmat('../data/LBO_basis.mat')
+
+    LBO_basis = LBO_BASIS['LBO_basis']
+    POD_basis = POD_BASIS['POD_basis']
+    POD_value = POD_BASIS['POD_value']
+    
+    LBO_MATRIX = torch.Tensor(LBO_basis[:,:Lmodes]).cuda()
+    LBO_INVERSE = (LBO_MATRIX.T@LBO_MATRIX).inverse()@LBO_MATRIX.T 
+    
+    print("\n======The data have been imported=======\n")
+    
+    spatial_basis = np.zeros((modes,nt))
+    if BASIS == 'POD':
+        
+        power = np.sum(POD_value[:modes])/np.sum(POD_value[:])
+        print('Energy proportion:',np.round(power,8),'%')
+        
+        for j in range(modes):
+            spatial_basis[j] = POD_basis[:,j]
+       
+        spatial_basis = torch.Tensor(spatial_basis).cuda()
+        
+        BASE_MATRIX = POD_basis[:,:modes].copy()
+        BASE_MATRIX = torch.Tensor(BASE_MATRIX).cuda()
+        BASE_INVERSE = torch.linalg.pinv(BASE_MATRIX)
+        print("\n===== POD basis calculation complete=====\n")
+    
+    if BASIS == 'LBO':
+        for j in range(modes):
+            spatial_basis[j] = LBO_MATRIX[:,j]
+        
+        spatial_basis = torch.Tensor(spatial_basis).cuda()
+        
+        BASE_MATRIX = LBO_MATRIX[:,:modes].copy()
+        BASE_MATRIX = torch.Tensor(BASE_MATRIX).cuda()
+        BASE_INVERSE = (BASE_MATRIX.T@BASE_MATRIX).inverse()@BASE_MATRIX.T 
+
+        print("\n======LBO basis calculation complete====\n")
+    
+    x_train = torch.Tensor(x_dataIn[:ntrain,:].reshape(ntrain,-1,1))
+    y_train = torch.Tensor(y_dataIn[:ntrain,:,:])
+    x_test  = torch.Tensor(x_dataIn[-ntest:,:].reshape(ntest,-1,1))
+    y_test  = torch.Tensor(y_dataIn[-ntest:,:,:])
+    
+    norm_x  = Z_score(x_train)
+    x_train = norm_x.encode(x_train)
+    x_test  = norm_x.encode(x_test)
+    
+    norm_y  = Z_score(y_train)
+    y_train = norm_y.encode(y_train)
+    y_test  = norm_y.encode(y_test)
+    
+
+    train_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_train, y_train), 
+                                               batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test), 
+                                              batch_size=batch_size, shuffle=False)
+
+
+    weight_model = NORM_Net(Lmodes,width,modes,LBO_MATRIX,LBO_INVERSE).cuda()
+    ################################################################
+    # training and evaluation
+    ################################################################
+    optimizer = torch.optim.Adam(weight_model.parameters(), lr=learning_rate, weight_decay=1e-4)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)
+    
+    myloss = L2Loss(size_average=False)
+    
+    time_start = time.perf_counter()
+    time_step = time.perf_counter()
+    
+    train_error = np.zeros((epochs))
+    test_error = np.zeros((epochs))
+    ET_list = np.zeros((epochs))
+    
+    for ep in range(epochs):
+        
+        weight_model.train()
+
+        train_l2 = 0
+        for x, y in train_loader:
+            x, y = x.cuda(), y.cuda()
+    
+            optimizer.zero_grad()
+            out_weight = weight_model(x)
+            out  = torch.einsum('ntm,mx->ntx',out_weight,spatial_basis)
+            l2 = myloss(out.view(batch_size, -1), y.view(batch_size, -1))
+            l2.backward() 
+            
+            # out_real = norm_y.decode(out.view(batch_size, -1).cpu())
+            # y_real = norm_y.decode(y.view(batch_size, -1).cpu())
+            
+            out_real = norm_y.decode(out.cpu())
+            y_real = norm_y.decode(y.cpu())
+            
+            train_l2 += myloss(out_real, y_real).item()   
+    
+            optimizer.step()
+
+        scheduler.step()
+        weight_model.eval()
+        test_l2 = 0.0
+        
+        with torch.no_grad():
+            for x, y in test_loader:
+                x, y = x.cuda(), y.cuda()
+    
+                out = weight_model(x)
+                out  = torch.einsum('ntm,mx->ntx',out,spatial_basis)
+                out_real = norm_y.decode(out.cpu())
+                y_real = norm_y.decode(y.cpu())
+                
+                test_l2 += myloss(out_real, y_real).item()                
+                loss_max_test = (abs(out.view(batch_size, -1)- y.view(batch_size, -1))).max(axis=1).values.mean()
+    
+        train_l2 /= ntrain
+        test_l2 /= ntest
+        train_error[ep] = train_l2
+        test_error[ep] = test_l2
+        
+        ET_list[ep] = loss_max_test
+        time_step_end = time.perf_counter()
+        T = time_step_end - time_step
+
+        print('Step: %d, Train L2: %.5f, Test L2: %.5f, Emax_test: %.5f, Time: %.3fs'%(ep, train_l2, test_l2, loss_max_test, T))
+        time_step = time.perf_counter()
+    
+    print("\n=============================")
+    print("Training done...")
+    print("=============================\n")
+    
+    test_loader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(x_test, y_test), 
+                                              batch_size=1, shuffle=False)
+    pre_test = torch.zeros(y_test.shape)
+    y_test   = torch.zeros(y_test.shape)
+    x_test   = torch.zeros(x_test.shape)
+    
+    index = 0
+    with torch.no_grad():
+        for x, y in test_loader:
+            x, y = x.cuda(), y.cuda()
+
+            out = weight_model(x)
+            out  = torch.einsum('ntm,mx->ntx',out,spatial_basis)
+            
+            out_real = norm_y.decode(out.cpu())
+            y_real   = norm_y.decode(y.cpu())
+            x_real   = norm_x.decode(x.cpu())
+            pre_test[index,:] = out_real
+            y_test[index,:] = y_real
+            x_test[index,:] = x_real
+            
+            index = index + 1
+            
+    Test_error  = np.abs(pre_test.cpu().detach().numpy() - y_test.cpu().detach().numpy())
+    T_max_t = np.max(Test_error)
+    T_max = np.max(Test_error[:, :, :], axis=(1, 2))
+    T_mean = np.mean(Test_error[:, :, :], axis=(1, 2))
+    
+    print('\nMaxMax:', np.round(T_max_t, 3))
+    print('\nMeanMax:', np.round(np.mean(T_max), 3), 'Std:', np.round(np.std(T_max),3))
+    print('MeanError:', np.round(np.mean(T_mean), 3), 'Std:', np.round(np.std(T_mean), 3))
+    
+    # ================ Save Data ====================
+    current_directory = os.getcwd()
+    sava_path = current_directory + "/logs/" + BASIS +"_"+ CaseName + "/"
+    if not os.path.exists(sava_path):
+        os.makedirs(sava_path)
+    
+    dataframe = pd.DataFrame({'Test_loss': [test_l2],
+                              'num_paras': [get_parameter_number(weight_model)],
+                              'train_time':[time_step_end - time_start]})
+    
+    dataframe.to_csv(sava_path + 'log.csv', index = False, sep = ',')
+    
+    loss_dict = {'train_error' :train_error,
+                 'test_error'  :test_error}
+    
+    pred_dict = {'pre_test' : pre_test.cpu().detach().numpy(),
+                    'x_test'   : x_test.cpu().detach().numpy(),
+                    'y_test'   : y_test.cpu().detach().numpy(),
+                    }
+    
+    sio.savemat(sava_path +'STFNO_loss_' + CaseName + '.mat', mdict = loss_dict)                                                     
+    sio.savemat(sava_path +'STFNO_pre_'  + CaseName + '.mat', mdict = pred_dict)
+    
+    print('\nTesting error: %.3e'%(test_l2))
+    print('Training time: %.3f'%(time_step_end - time_start))
+    print('Num of paras : %d'%(get_parameter_number(weight_model)))
+        
+
+
+if __name__ == "__main__":
+    
+    class objectview(object):
+        def __init__(self, d):
+            self.__dict__ = d
+            
+    for i in range(3):
+        
+        i = i + 1
+        for args in [
+                
+                { 'modes': 32, 
+                  'LBOmodes' :128,
+                  'width': 64,
+                  'batch_size': 50, 
+                  'epochs': 500,
+                  'data_dir': '../data/Burgers', 
+                  'num_train': 3000, 
+                  'num_test': 500,
+                  'casename': 'Burgers_'+str(i),
+                   'BASIS':'POD',
+                    # 'BASIS':'LBO',
+                  'lr' : 0.01},
+             ]:
+            
+            args = objectview(args)
+                    
+        main(args)
---
+++ utils.py
@@ -0,0 +1,189 @@
+
+import time
+from timeit import default_timer
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# from torch.optim import Adam
+from functools import reduce
+import operator
+from Adam import Adam
+
+#################################################
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class Approximation_block(nn.Module):
+    def __init__ (self, in_channels, out_channels, modes, LBO_MATRIX, LBO_INVERSE):
+        
+        super(Approximation_block, self).__init__()
+        
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = LBO_MATRIX.shape[1]
+        self.LBO_MATRIX = LBO_MATRIX
+        self.LBO_INVERSE = LBO_INVERSE
+
+        self.scale = (1 / (in_channels*out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, dtype=torch.float))
+
+    def forward(self, x):
+                        
+        ################################################################
+        # Encode
+        ################################################################
+        x = x = x.permute(0, 2, 1)
+        x = self.LBO_INVERSE @ x  
+        x = x.permute(0, 2, 1)
+        
+        ################################################################
+        # Approximator
+        ################################################################
+        x = torch.einsum("bix,iox->box", x[:, :], self.weights1)
+        
+        ################################################################
+        # Decode
+        ################################################################
+        x =  x @ self.LBO_MATRIX.T
+        
+        return x
+    
+class NORM_Net(nn.Module):
+    def __init__(self, modes, width, channel, LBO_MATRIX, LBO_INVERSE):
+        super(NORM_Net, self).__init__()
+
+        self.modes1 = modes
+        self.width = width
+        self.channel = channel
+        self.padding = 2 
+        self.fc0 = nn.Linear(1, self.width) 
+        self.LBO_MATRIX = LBO_MATRIX
+        self.LBO_INVERSE = LBO_INVERSE
+
+        self.conv0 = Approximation_block(self.width, self.width, self.modes1, self.LBO_MATRIX, self.LBO_INVERSE )
+        self.conv1 = Approximation_block(self.width, self.width, self.modes1, self.LBO_MATRIX, self.LBO_INVERSE )
+        self.conv2 = Approximation_block(self.width, self.width, self.modes1, self.LBO_MATRIX, self.LBO_INVERSE )
+        self.conv3 = Approximation_block(self.width, self.width, self.modes1, self.LBO_MATRIX, self.LBO_INVERSE )
+
+        
+        self.w0 = nn.Conv1d(self.width, self.width, 1)
+        self.w1 = nn.Conv1d(self.width, self.width, 1)
+        self.w2 = nn.Conv1d(self.width, self.width, 1)
+        self.w3 = nn.Conv1d(self.width, self.width, 1)
+
+
+        self.fc1 = nn.Linear(self.width, 32)
+        self.fc2 = nn.Linear(32, self.channel)
+
+    def forward(self, x):
+
+        x = self.fc0(x)
+        x = x.permute(0, 2, 1)
+        
+        x1 = self.conv0(x)
+        x2 = self.w0(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.conv1(x)
+        x2 = self.w1(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.conv2(x)
+        x2 = self.w2(x)
+        x = x1 + x2
+        x = F.gelu(x)
+
+        x1 = self.conv3(x)
+        x2 = self.w3(x)
+        x = x1 + x2
+
+        x = x.permute(0, 2, 1)
+        x = self.fc1(x)
+        x = F.gelu(x)
+        x = self.fc2(x)
+        return x
+    
+    
+
+class Z_score(object):
+    def __init__(self, x, eps=0.00001):
+        super(Z_score, self).__init__()
+        
+        # x could be in shape of ntrain*time or ntrain*time*nodes
+        self.mean = torch.mean(x, 0)
+        self.std = torch.std(x, 0)
+        self.eps = eps
+
+    def encode(self, x):
+        
+        x = (x - self.mean) / (self.std + self.eps)
+        return x
+
+    def decode(self, x):
+        
+        std = self.std + self.eps 
+        mean = self.mean
+
+        x = (x * std) + mean
+        return x
+
+class L2Loss(object):
+    
+    def __init__(self, d=2, size_average=True):
+        super(L2Loss, self).__init__()
+        
+        # Dimension and Lp-norm type are postive
+        assert d > 0
+        self.d = d
+        self.p = 2
+        self.size_average = size_average
+
+
+    def rel(self, x, y):
+        
+        num_examples = x.size()[0]
+        diff_norms = torch.norm(x.reshape(num_examples, -1) - y.reshape(num_examples, -1), self.p, 1)
+        y_norms = torch.norm(y.reshape(num_examples, -1), self.p, 1)
+
+        if self.size_average:
+            return torch.mean(diff_norms / y_norms)
+        else:
+            return torch.sum(diff_norms / y_norms)
+
+
+    def __call__(self, x, y):
+        return self.rel(x, y)
+
+class MinMax(object):
+    
+    def __init__(self, x, low=0.0, high=1.0):
+        super(MinMax, self).__init__()
+        mymin = torch.min(x, 0)[0].view(-1)
+        print('Min',mymin.shape)
+        mymax = torch.max(x, 0)[0].view(-1)
+        print('Max',mymax.shape)
+
+        self.a = (high - low)/(mymax - mymin)
+        self.b = -self.a*mymax + high
+
+    def encode(self, x):
+        s = x.size()
+        x = x.view(s[0], -1)
+        x = self.a*x + self.b
+        x = x.view(s)
+        return x
+
+    def decode(self, x):
+        s = x.size()
+        x = x.view(s[0], -1)
+        x = (x - self.b)/self.a
+        x = x.view(s)
+        return x
+    
+def get_parameter_number(net):
+    
+    trainable_num = sum(p.numel() for p in net.parameters() if p.requires_grad)
+    return  trainable_num
